bc3b651cd2123207cefee20f0df52311db0e59cc
ManaliNarkhede <manalinarkhede27@gmail.com>
2026-06-20 00:22:48 +0530

    cookies added, auth route created

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+
+// POST /api/auth/register
+async function userRegisterController(req, res) {
+    const { email, name, password } = req.body
+    const isExists = await userModel.findOne({ email : email })
+
+    if (isExists) {
+        return res.status(400).json({
+            status : "failed",
+            message : "User already exists"
+        })
+    }
+    const user = await userModel.create({ email, name, password })
+    const token = jwt.sign({ userId : user._id }, process.env.JWT_SECRET, { expiresIn : "3d" })
+    res.cookie("token", token)
+    res.status(201).json({
+        user : {
+            email : user.email,
+            _id : user._id,
+            name : user.name
+        },
+        token : token
+    })
+}
+
+module.exports = {
+    authController: {
+        userRegisterController
+    }
+}
