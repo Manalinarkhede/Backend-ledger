29aa50b44b8103945633706c4e61d7c75b27c38e
ManaliNarkhede <manalinarkhede27@gmail.com>
2026-06-20 10:23:17 +0530

    login api created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 
-// POST /api/auth/register
+/**
+ * - POST /api/auth/register
+ */
 async function userRegisterController(req, res) {
     const { email, name, password } = req.body
     const isExists = await userModel.findOne({ email : email })
@@ -25,8 +27,44 @@ async function userRegisterController(req, res) {
     })
 }
 
+/**
+ * - POST /api/auth/login
+ */
+async function userLoginController(req, res) {
+    const { email, password } = req.body
+    const user = await userModel.findOne({ email : email }).select("+password")
+
+    if (!user) {
+        return res.status(400).json({
+            status : "failed",
+            message : "Invalid email or password"
+        })
+    }
+
+    const isValidPassword = await user.comparePassword(password)
+
+    if (!isValidPassword) {
+        return res.status(400).json({
+            status : "failed",
+            message : "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({ userId : user._id }, process.env.JWT_SECRET, { expiresIn : "3d" })
+    res.cookie("token", token)
+    res.status(200).json({
+        user : {
+            email : user.email,
+            _id : user._id,
+            name : user.name
+        },
+        token : token
+    })
+}
+
 module.exports = {
     authController: {
-        userRegisterController
+        userRegisterController,
+        userLoginController
     }
 }
